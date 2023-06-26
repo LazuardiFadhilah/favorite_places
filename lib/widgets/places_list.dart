937e7c04cfeecdcfe8ebd93823e6680ea5b42f44
Lazuardi Fadhilah <Lazuardifadhillah@gmail.com>
2023-06-26 20:43:53 +0700

    adding place details creen

--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -1,3 +1,4 @@
+import 'package:favorite_places/screens/place_detail.dart';
 import 'package:flutter/material.dart';
 
 import '../models/place.dart';
@@ -10,19 +11,32 @@ class PlacesList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (places.isEmpty) {
-      return const Center(
-        child: Text('No Places added yet'),
+      return Center(
+        child: Text(
+          'No Places added yet',
+          style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
       );
     }
     return ListView.builder(
-        itemCount: places.length,
-        itemBuilder: (ctx, index) => ListTile(
-              title: Text(
-                places[index].title,
-                style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                      color: Theme.of(context).colorScheme.onBackground,
-                    ),
+      itemCount: places.length,
+      itemBuilder: (ctx, index) => ListTile(
+        title: Text(
+          places[index].title,
+          style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
               ),
-            ));
+        ),
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (ctx) => PlaceDetailSCreen(place: places[index]),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
